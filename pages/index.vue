f8ce855c7b4b62402aaeabad1c71047310a4ddc6
kozkoz <kozuka.kosuke.18@shizuoka.ac.jp>
2021-06-28 16:22:52 +0900

    とりあえず時間取得

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>index.vueです</div>
+  <div>{{ thisMinute }}</div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      time: 0,
+      thisMinute: 0,
+    };
+  },
+  mounted() {
+    this.thisMinute = this.timeCheck();
+  },
+  methods: {
+    timeCheck() {
+      const date1 = new Date();
+      const minute = date1.getMinutes();
+      console.log(minute);
+      return minute;
+    },
+    rescueRequest() {
+      console.log('わからん！！');
+    },
+  },
+};
 </script>
