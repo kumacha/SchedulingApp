3efa66793306330740149fd89f714ae291459af8
kozkoz <kozuka.kosuke.18@shizuoka.ac.jp>
2021-07-04 04:34:53 +0900

    git commit が60分なかった場合にhelpがtrueになり天国と地獄が流れる。俺はデータソンに行くぞ

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,29 @@
 </template>
 <script>
 import firebase from '@/plugins/firebase';
+import { Octokit } from '@octokit/core';
 export default {
   data() {
     return {
-      time: 0,
-      help: false,
+      time: 0, // 時間(1time = 2分)
+      help: false, // ヘルプサインの有無
+      commitTime: '2021-07-03T18:54:27Z', // gitにcommitした時間
+      gitName: 'kozkoz', // gitのUser名
     };
   },
   mounted() {
-    setInterval(this.rescueRequest, 1000000);
+    this.commitTime = this.gitUser();
+    setInterval(this.rescueRequest, 120000);
   },
   methods: {
     rescueRequest() {
       this.time += 1;
-      if (this.time > 0) {
+      const lastCommitTime = this.gitUser();
+      if (lastCommitTime !== this.commitTime) {
+        this.commitTime = lastCommitTime;
+        this.time = 0;
+      }
+      if (this.time > 30) {
         this.help = true;
         const play = () => {
           // firebaseから音源データをダウンロード
@@ -29,6 +38,17 @@ export default {
         };
         play();
       }
+    },
+    async gitUser() {
+      // npm install @octokit/core を予め実行すること
+      const octokit = new Octokit({
+        auth: `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`,
+      });
+      const res = await octokit.request('GET /users/{org}/events', {
+        org: this.gitName,
+      });
+      console.log(res.data[0].created_at);
+      return res.data[0].created_at;
     },
   },
 };
